575dc8c9aca9362d445310da0c07d7840600c020
wpchop <wpchop@gmail.com>
2015-12-01 16:57:34 -0500

    typo on BLOCK_WIDTH

--- field.hpp
+++ field.hpp
@@ -5,7 +5,7 @@ class Painter;
 class Field {
 public:
   enum { WIDTH = 80, HEIGHT = 80 };
-  enum { BLOCK_wIDTH = 3, BLOCK_HEIGHT = 3 };
+  enum { BLOCK_WIDTH = 3, BLOCK_HEIGHT = 3 };
   enum Type { EMPTY = 0, PLAYER1 = 1, PLAYER2 = 2 };
   Field();
   void setBlock(Type, int x, int y);
